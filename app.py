4bdb97818e520e9233cf8f6df61459d93654fec5
Wakeel Khan <wakilkhn4697@gmail.com>
2026-05-29 11:31:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from docx import Document
 def transcribe_video(video_path):
     try:
         audio_path = convert_video_to_audio(video_path)
-        model = whisper.load_model("base")
+        model = whisper.load_model("medium")
         result = model.transcribe(audio_path)
         return result["text"]
     except Exception as e:
